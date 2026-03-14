01f91a7b3ee3be2e13659e8dd6ae152f0a2657ae
Rayan <rayansayyed.rs@gmail.com>
2026-03-14 22:37:29 +0300

    Merged updates between my commits and Ali's

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const authRoutes = require('./authRoutes');
 const adminRoutes = require('./adminRoutes');
 const roleRoutes = require('./roleRoutes');
+const categoryRoutes = require('./categoryRoutes');
+const governorateRoutes = require('./governorateRoutes');
+const branchRoutes = require('./branchRoutes');
+
 const router = express.Router();
 
-
+router.use('/api',categoryRoutes)
+router.use('/api', governorateRoutes)
+router.use('/api', branchRoutes)
 router.use('/api', roleRoutes);
 router.use('/api', authRoutes);
 router.use('/api', adminRoutes);
