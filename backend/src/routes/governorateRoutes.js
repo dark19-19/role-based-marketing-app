a35ae24c7bedd4d899be4a45a241ff47a30ac44b
Yazan.333.222.111 <yazanghy04@gmail.com>
2026-03-28 20:57:34 +0300

    edits the requireRole of getGovernorate

--- backend/src/routes/governorateRoutes.js
+++ backend/src/routes/governorateRoutes.js
@@ -1,36 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const governorateController = require('../controllers/governorateController');
-const authMiddleware = require('../middleware/auth');
-const requireRole = require('../middleware/roleMiddleware');
+const governorateController = require("../controllers/governorateController");
+const authMiddleware = require("../middleware/auth");
+const requireRole = require("../middleware/roleMiddleware");
 
 router.post(
-    '/governorates',
-    authMiddleware,
-    requireRole(['ADMIN']),
-    governorateController.create
+  "/governorates",
+  authMiddleware,
+  requireRole(["ADMIN"]),
+  governorateController.create,
 );
 
 router.put(
-    '/governorates/:id',
-    authMiddleware,
-    requireRole(['ADMIN']),
-    governorateController.update
+  "/governorates/:id",
+  authMiddleware,
+  requireRole(["ADMIN"]),
+  governorateController.update,
 );
 
 router.delete(
-    '/governorates/:id',
-    authMiddleware,
-    requireRole(['ADMIN']),
-    governorateController.delete
+  "/governorates/:id",
+  authMiddleware,
+  requireRole(["ADMIN"]),
+  governorateController.delete,
 );
 
 router.get(
-    '/governorates',
-    authMiddleware,
-    requireRole(['ADMIN']),
-    governorateController.list
+  "/governorates",
+  authMiddleware,
+  requireRole([
+    "ADMIN",
+    "GENERAL_SUPERVISOR",
+    "SUPERVISOR",
+    "MARKETER",
+    "BRANCH_MANAGER",
+  ]),
+  governorateController.list,
 );
 
 module.exports = router;
