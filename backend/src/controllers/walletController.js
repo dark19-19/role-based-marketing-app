3636519f5ff06a7e9b853dfd06c7e437e0a326d2
Yazan12104 <yazanghy04@gmail.com>
2026-05-07 02:32:21 +0300

    update : make the roles in if statment can git the Summary wallet in his targetUser

--- backend/src/controllers/walletController.js
+++ backend/src/controllers/walletController.js
@@ -3,7 +3,11 @@ const walletService = require('../services/walletService');
 class WalletController {
     async getSummary(req, res) {
         try {
-            const result = await walletService.getSummary(req.user);
+            let targetUser = req.user;
+            if (req.user.role === 'ADMIN' || "BRANCH_MANAGER" || "GENERAL_SUPERVISOR" || "SUPERVISOR" && req.query.userId) {
+                targetUser = { id: req.query.userId };
+            }
+            const result = await walletService.getSummary(targetUser);
             res.json({
                 success: true,
                 data: result
